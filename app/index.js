6d0f96337356abdd8325dd84c0db4e66439c6f0e
German Utz <germanutz@gmail.com>
2016-07-16 18:45:11 -0300

    codigo base para trabajar en clase3

--- app/index.js
+++ app/index.js
@@ -8,54 +8,14 @@ class SimpleApplication extends Component {
   constructor(props){
     super(props);
     this.state = {
-      value : 0
+
     }
-  }
-
-  handleSumar(){
-    this.setState({value : this.state.value+1})
-  }
-
-  handleRestar(){
-    this.setState({value : this.state.value-1})
   }
 
   render() {
     return (
       <div>
-        <Label valor = {this.state.value}/>
-        <ButtonSumar operacionSumar={this.handleSumar.bind(this)}/>
-        <ButtonRestar operacionRestar={this.handleRestar.bind(this)}/>
-      </div>
-    );
-  }
-}
 
-class Label extends Component {
-  render (){
-    return (
-      <div>
-        <label>El valor del contador es: {this.props.valor} </label>
-      </div>
-    );
-  }
-}
-
-class ButtonSumar extends Component {
-  render () {
-    return (
-      <div>
-        <button type="submit" onClick={this.props.operacionSumar} >Sumar </button>
-      </div>
-    );
-  }
-}
-
-class ButtonRestar extends Component {
-  render () {
-    return (
-      <div>
-        <button type="submit" onClick={this.props.operacionRestar} >Restar </button>
       </div>
     );
   }
