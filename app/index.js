0a7bb564ed0c9c808d2ca98bece2cc2205737b30
German Utz <germanutz@gmail.com>
2016-07-17 10:43:29 -0300

    resuelto ejercicio de cambiar mensaje apretando botton

--- app/index.js
+++ app/index.js
@@ -7,15 +7,67 @@ class SimpleApplication extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
 
+    this.state = {
+      estado : true
     }
+  }//end constructor
+
+  handleBorrarMensaje () {
+    this.setState({estado:false});
+    return this.state.estado;
+  }
+
+  handleMostarMensaje () {
+    this.setState({estado:true});
+    return this.state.estado;
   }
 
   render() {
-    return (
-      <div>
 
+    return (
+
+      <div>
+            <Label  estadoActual = {this.state.estado} />
+            <Button borrarMensaje = {this.handleBorrarMensaje.bind(this)}
+                    mostrarMensaje = {this.handleMostarMensaje.bind(this)} />
+
+      </div>
+
+    );
+  }//end render
+}//end SimpleApplication
+
+class Label extends Component {
+
+  render () {
+
+    let estado = this.props.estadoActual;
+    console.log("el estado es :" + estado);
+    let clase = "";
+
+
+    if(estado){
+        clase = "inline";
+    }else{
+        clase = "none";
+    }
+
+    return(
+      <div>
+        <span style = {{display : clase }}> Hola </span>
+      </div>
+    );
+  }
+}// en Label
+
+class Button extends Component {
+
+  render(){
+    return(
+      <div>
+        <button type="submit" onClick = {this.props.borrarMensaje}>Borrar</button>
+        <button type="sumbit" onClick = {this.props.mostrarMensaje}>Mostrar </button>
       </div>
     );
   }
