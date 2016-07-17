a4f38a342faeb5e227e62b57966c313c1b34978a
German Utz <germanutz@gmail.com>
2016-07-17 18:34:19 -0300

    falto capturar componente no controlado

--- app/index.js
+++ app/index.js
@@ -14,31 +14,59 @@ constructor (props) {
 
 handleValueChange (event) {
   this.setState({value : event.target.value});
+}
+
+handlePrintImput (){
   console.log(this.state.value);
+  {/*Donde capturo este imput me da undefined*/}
+  if(this._input !== null){
+    console.log(this._input);
+  }
+
 }
 
 render() {
   return (
     <div>
-        <Input inputValueDefault = {this.state.value}
-               valueChange = {this.handleValueChange.bind(this)}
-        />
+      <InputControlado defaultValue = {this.state.value}
+                       changeValue = {this.handleValueChange.bind(this)}/>
+      <InputNoControlado />
+      <Button  printInput = {this.handlePrintImput.bind(this)}/>
     </div>
   );
 }
 
 }//end SimpleApplication
 
-export default class Input extends Component {
+export default class InputControlado extends Component {
   render (){
-
     return (
       <div>
-        <input type="text" value = {this.props.inputValueDefault} onChange = {this.props.valueChange} />
+        <input type="text" value={this.props.defaultValue} onChange={this.props.changeValue}/>
       </div>
     );
   }
+}
 
+
+export default class InputNoControlado extends Component {
+  render(){
+    return (
+      <div>
+        <input type="text" ref={(c) => this._input = c} />
+      </div>
+    );
+  }
+}
+
+export default class Button extends Component {
+  render(){
+    return(
+      <div>
+        <button type="submit" onClick={this.props.printInput}>Enviar </button>
+      </div>
+    );
+  }
 }
 
 
