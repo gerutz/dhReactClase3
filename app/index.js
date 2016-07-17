5c3d715165116c78c2bbc8376931a6490c28e413
German Utz <germanutz@gmail.com>
2016-07-17 11:08:49 -0300

    preparacion ejercicio 3.3 y 3.4

--- app/index.js
+++ app/index.js
@@ -9,66 +9,48 @@ class SimpleApplication extends Component {
     super(props);
 
     this.state = {
-      estado : true
     }
   }//end constructor
 
-  handleBorrarMensaje () {
-    this.setState({estado:false});
-    return this.state.estado;
-  }
-
-  handleMostarMensaje () {
-    this.setState({estado:true});
-    return this.state.estado;
-  }
-
   render() {
-
     return (
-
       <div>
-            <Label  estadoActual = {this.state.estado} />
-            <Button borrarMensaje = {this.handleBorrarMensaje.bind(this)}
-                    mostrarMensaje = {this.handleMostarMensaje.bind(this)} />
-
+            <Label  />
+            <Texto />
+            <Button />
       </div>
-
     );
-  }//end render
+  }
+
 }//end SimpleApplication
 
 class Label extends Component {
 
   render () {
-
-    let estado = this.props.estadoActual;
-    console.log("el estado es :" + estado);
-    let clase = "";
-
-
-    if(estado){
-        clase = "inline";
-    }else{
-        clase = "none";
-    }
-
     return(
       <div>
-        <span style = {{display : clase }}> Hola </span>
+        <label>DEMO</label>
       </div>
     );
   }
-}// en Label
+}
+
+class Texto extends Component {
+
+  render(){
+    return(
+      <div>
+        <p>Demo texto</p>
+      </div>
+    );
+  }
+}
 
 class Button extends Component {
 
   render(){
     return(
-      <div>
-        <button type="submit" onClick = {this.props.borrarMensaje}>Borrar</button>
-        <button type="sumbit" onClick = {this.props.mostrarMensaje}>Mostrar </button>
-      </div>
+      <button type="submit">Enviar</button>
     );
   }
 }
