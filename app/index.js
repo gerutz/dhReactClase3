152defb96828427e9aba7a7a9d5859d6f4237a91
German Utz <germanutz@gmail.com>
2016-07-17 17:32:49 -0300

    ejercicio onChange en input text

--- app/index.js
+++ app/index.js
@@ -3,99 +3,43 @@ import ReactDOM from 'react-dom';
 
 const content = document.getElementById('content');
 
-class SimpleApplication extends Component {
+export default class SimpleApplication extends Component {
 
-  constructor(props){
-    super(props);
-
-    this.state = {
-      inicial : 0,
-      actual : 0
-    }
+constructor (props) {
+  super(props);
+  this.state = {
+    value : "Hola Mundo"
   }
+}
 
-  handleAgregarNumero () {
-    return this.setState({actual : this.state.actual+1});
-    console.log(this.state.actual);
-  }
+handleValueChange (event) {
+  this.setState({value : event.target.value});
+  console.log(this.state.value);
+}
 
-  handleEliminarNumero () {
-    return this.setState({actual : this.state.actual-1});
-    console.log(this.state.actual);
-  }
-
-  handleResetNumero () {
-    return this.setState({actual : 0});
-  }
-
-  render() {
-    return (
-      <div>
-            <Label estadoInicial = {this.state.actual}
-            />
-            <Texto estadoInicial = {this.state.inicial}
-                   estadoActual = {this.state.actual}
-            />
-            <Button agregarNumero = {this.handleAgregarNumero.bind(this)}
-                    eliminarNumero = {this.handleEliminarNumero.bind(this)}
-                    resetNumero = {this.handleResetNumero.bind(this)}
-            />
-      </div>
-    );
-  }
+render() {
+  return (
+    <div>
+        <Input inputValueDefault = {this.state.value}
+               valueChange = {this.handleValueChange.bind(this)}
+        />
+    </div>
+  );
+}
 
 }//end SimpleApplication
 
-class Label extends Component {
+export default class Input extends Component {
+  render (){
 
-  render () {
-
-    return(
+    return (
       <div>
-        <label>El numero actual es  {this.props.estadoInicial}</label>
+        <input type="text" value = {this.props.inputValueDefault} onChange = {this.props.valueChange} />
       </div>
     );
   }
+
 }
 
-class Texto extends Component {
-
-  render(){
-
-    let calcularModulo = (valor) => {
-      if(valor === 0){
-        return "Cero";
-      }else if(valor % 2 == 0){
-        return "Par";
-      }else{
-        return "Impar";
-      };
-    };
-
-    let val = this.props.estadoActual;
-
-    let resultado = calcularModulo(val);
-
-    return(
-      <div>
-        <p>{resultado}</p>
-      </div>
-    );
-  }
-}
-
-class Button extends Component {
-
-  render(){
-    return(
-      <div>
-      <button type="submit" onClick = {this.props.agregarNumero}>Sumar</button>
-      <button type="submit" onClick = {this.props.eliminarNumero}>Restar</button>
-      <button type="submit" onClick = {this.props.resetNumero}>Resetear</button>
-      </div>
-
-    );
-  }
-}
 
 ReactDOM.render(<SimpleApplication />, content);
